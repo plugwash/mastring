3b3d5186a3b9aea2b6aa41d038b26c763bfb57a3
Peter Michael Green <plugwash@debian.org>
2025-03-29 22:51:33 +0000

    Add "example" showing use of the code.

--- examples/demo.rs
+++ examples/demo.rs
@@ -2,4 +2,33 @@ use mastring::MAString;
 fn main() {
     let foo = MAString::from_static("hello");
     println!("{} {}",foo,foo.getMode());
+    let bar = foo.clone();
+    println!("{} {}",foo,foo.getMode());
+    println!("{} {}",bar,bar.getMode());
+
+    let foo = MAString::from_static("the quick brown fox jumped over the lazy dog");
+    println!("{} {}",foo,foo.getMode());
+    let bar = foo.clone();
+    println!("{} {}",foo,foo.getMode());
+    println!("{} {}",bar,bar.getMode());
+
+    let foo = MAString::from_slice("hello");
+    println!("{} {}",foo,foo.getMode());
+    let bar = foo.clone();
+    println!("{} {}",foo,foo.getMode());
+    println!("{} {}",bar,bar.getMode());
+
+    let foo = MAString::from_slice("the quick brown fox jumped over the lazy dog");
+    println!("{} {}",foo,foo.getMode());
+    let bar = foo.clone();
+    println!("{} {}",foo,foo.getMode());
+    println!("{} {}",bar,bar.getMode());
+
+    let foo = MAString::from_string("the quick brown fox jumped over the lazy dog".to_string());
+    println!("{} {}",foo,foo.getMode());
+    let bar = foo.clone();
+    println!("{} {}",foo,foo.getMode());
+    println!("{} {}",bar,bar.getMode());
+
+
 }
